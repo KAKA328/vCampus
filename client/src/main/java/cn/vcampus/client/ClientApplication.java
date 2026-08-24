b9b4c4c41e7f28c58d6773e6f0add1203119dfe0
KAKA328 <260883147@qq.com>
2026-08-25 00:49:55 +0800

    feat(client): demonstrate user session lifecycle

--- client/src/main/java/cn/vcampus/client/ClientApplication.java
+++ client/src/main/java/cn/vcampus/client/ClientApplication.java
@@ -2,6 +2,10 @@ package cn.vcampus.client;
 
 import cn.vcampus.common.Message;
 import cn.vcampus.common.MessageType;
+import cn.vcampus.common.StatusCode;
+import cn.vcampus.user.AuthorizationRequest;
+import cn.vcampus.user.Permission;
+import cn.vcampus.user.Session;
 import cn.vcampus.user.UserCredentials;
 
 import java.io.IOException;
@@ -19,16 +23,51 @@ public final class ClientApplication {
         try (Socket socket = new Socket(host, port);
              ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
              ObjectInputStream input = new ObjectInputStream(socket.getInputStream())) {
-            UserCredentials demo = new UserCredentials("demo-student", "demo123", "Demo Student", "STUDENT");
-            output.writeObject(Message.request("startup-register", MessageType.REGISTER, demo));
-            output.flush();
-            Message registerResponse = (Message) input.readObject();
-            System.out.println("register response: " + registerResponse.getStatusCode());
+            UserCredentials demo = new UserCredentials("demo-student-" + System.currentTimeMillis(),
+                    "demo123", "Demo Student", "STUDENT");
 
-            output.writeObject(Message.request("startup-login", MessageType.LOGIN, demo));
-            output.flush();
-            Message loginResponse = (Message) input.readObject();
-            System.out.println("login response: " + loginResponse.getStatusCode());
+            Message registerResponse = exchange(output, input,
+                    Message.request("demo-register", MessageType.REGISTER, demo));
+            printResult(registerResponse, "REGISTER", StatusCode.OK);
+
+            Message loginResponse = exchange(output, input,
+                    Message.request("demo-login", MessageType.LOGIN, demo));
+            printResult(loginResponse, "LOGIN", StatusCode.OK);
+            Session session = requireSession(loginResponse);
+
+            Message authorizeResponse = exchange(output, input,
+                    Message.request("demo-authorize-course-select", MessageType.AUTHORIZE,
+                            new AuthorizationRequest(session.getToken(), Permission.COURSE_SELECT.getCode())));
+            printResult(authorizeResponse, "AUTHORIZE COURSE_SELECT", StatusCode.OK);
+
+            Message logoutResponse = exchange(output, input,
+                    Message.request("demo-logout", MessageType.LOGOUT, session.getToken()));
+            printResult(logoutResponse, "LOGOUT", StatusCode.OK);
+
+            Message oldTokenResponse = exchange(output, input,
+                    Message.request("demo-authorize-old-token", MessageType.AUTHORIZE,
+                            new AuthorizationRequest(session.getToken(), Permission.COURSE_SELECT.getCode())));
+            printResult(oldTokenResponse, "AUTHORIZE OLD_TOKEN", StatusCode.UNAUTHORIZED);
         }
+    }
+
+    private static Message exchange(ObjectOutputStream output, ObjectInputStream input, Message request)
+            throws IOException, ClassNotFoundException {
+        output.writeObject(request);
+        output.flush();
+        return (Message) input.readObject();
+    }
+
+    private static Session requireSession(Message loginResponse) {
+        if (!(loginResponse.getPayload() instanceof Session)) {
+            throw new IllegalStateException("login did not return a session");
+        }
+        return (Session) loginResponse.getPayload();
+    }
+
+    private static void printResult(Message response, String operation, StatusCode expected) {
+        System.out.println(response.getRequestId() + " " + operation
+                + " actual=" + response.getStatusCode()
+                + " expected=" + expected);
     }
 }
